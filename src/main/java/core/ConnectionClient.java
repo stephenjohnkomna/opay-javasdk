ca2c0dffba7494a8feb8d6905e338ae88b63a019
stephenjohnkomna <stephenjohn.tech@gmail.com>
2025-09-21 18:12:52 -0500

    Inquiry Class documentation

--- src/main/java/core/ConnectionClient.java
+++ src/main/java/core/ConnectionClient.java
@@ -1,27 +1,42 @@
 package core;
-
-import com.google.gson.Gson;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
 import org.json.JSONObject;
-
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
 
+
+/**
+ * ConnectionClient Class.
+ */
 public class ConnectionClient {
+    /**
+     * baseUrl
+     */
     private String baseUrl;
+
+
+    /**
+     * headers
+     */
     private Map<String,String> headers= new HashMap<String,String>();
 
+    /**
+     * Constructor for Client Connection
+     */
     public ConnectionClient(String baseUrl,HashMap<String,String> headers) {
         this.baseUrl = baseUrl;
         this.headers = headers;
     }
 
+
+    /**
+     * Post Request
+     */
     public JSONObject makePostRequest(TreeMap<String, Object> parameters, String endpoint) {
         try {
             HttpResponse<JsonNode> result =
@@ -36,6 +51,10 @@ public class ConnectionClient {
         return null;
     }
 
+
+    /**
+     * Post Request
+     */
     public JSONObject makePostRequest(String endpoint) {
         try {
             HttpResponse<JsonNode> result =
--- src/main/java/core/module/Inquiry.java
+++ src/main/java/core/module/Inquiry.java
@@ -5,27 +5,48 @@ import core.Endpoint;
 import org.json.JSONObject;
 import java.util.TreeMap;
 
+/**
+ * Inquiry Class
+ */
 public class Inquiry {
 
+    /**
+     * Inquiry Class
+     */
     private ConnectionClient connectionClient;
 
+    /**
+     * Inquiry Constructor
+     */
     public Inquiry(ConnectionClient connectionClient)
     {
         this.connectionClient = connectionClient;
     }
 
+    /**
+     * balanceForAllAccount
+     */
     public JSONObject balanceForAllAccount() {
         return this.connectionClient.makePostRequest(Endpoint.OPAY_INQUIRY_BALANCE_FOR_ALL_ACCOUNT);
     }
 
+    /**
+     * validateMerchant
+     */
     public JSONObject validateMerchant(TreeMap<String, Object> param) {
         return this.connectionClient.makePostRequest(param,Endpoint.OPAY_INQUIRY_VALIDATE_MERCHANT);
     }
 
+    /**
+     * validateUser
+     */
     public JSONObject validateUser(TreeMap<String, Object> param) {
         return this.connectionClient.makePostRequest(param,Endpoint.OPAY_INQUIRY_VALIDATE_USER);
     }
 
+    /**
+     * verifyAccountAndReturnAllocatedAccountName
+     */
     public JSONObject verifyAccountAndReturnAllocatedAccountName(TreeMap<String, Object> param) {
         return this.connectionClient.makePostRequest(param,Endpoint.OPAY_INQUIRY_VERIFY_ACCOUNT_AND_RETURN_ALLOCATEDACCOUNTNAME);
     }
